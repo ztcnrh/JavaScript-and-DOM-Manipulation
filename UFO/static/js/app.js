aea10b4bb5d703a03f6d6bb1d7c6f01de4962085
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-04-06 23:17:43 -0400

    Step 1: Display data Update app.js to display all the UFO sightings data. Next step: build filters...

--- UFO/static/js/app.js
+++ UFO/static/js/app.js
@@ -1,4 +1,72 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+
+// ----------------------------------------------
+// Create event listeners to find rows that match user input as filters.
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+// var form = d3.select("form");
+
+// // Create event handlers 
+// button.on("click", runEnter);
+// form.on("submit",runEnter);
+
+// // Complete the event handler function for the form
+// function runEnter() {
+
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
+  
+//   // Select the input element and get the raw HTML node
+//   var inputElement = d3.select("#datetime");
+
+//   // Get the value property of the input element
+//   var inputValue = inputElement.property("value");
+
+//   console.log(inputValue);
+//   console.log(people);
+
+//   var filteredData = people.filter(person => person.bloodType === inputValue);
+
+//   console.log(filteredData);
+
+//   // BONUS: Calculate summary statistics for the age field of the filtered data
+
+//   // First, create an array with just the age values
+//   var ages = filteredData.map(person => person.age);
+
+//   // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
+//   var mean = math.mean(ages);
+//   var median = math.median(ages);
+//   var mode = math.mode(ages);
+//   var variance = math.variance(ages);
+//   var standardDeviation = math.std(ages);
+
+//   // Then, select the unordered list element by class name
+//   var list = d3.select(".summary");
+
+//   // remove any children from the list to
+//   list.html("");
+
+//   // append stats to the list
+//   list.append("li").text(`Mean: ${mean}`);
+//   list.append("li").text(`Median: ${median}`);
+//   list.append("li").text(`Mode: ${mode}`);
+//   list.append("li").text(`Variance: ${variance}`);
+//   list.append("li").text(`Standard Deviation: ${standardDeviation}`);
+// };
