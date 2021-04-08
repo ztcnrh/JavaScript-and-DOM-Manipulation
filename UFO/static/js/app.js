d0e1f5e05987c95526f57b7a7f817d6daf01e374
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-04-08 00:12:54 -0400

    First update with filters Need further improvements so that it can be filtered by only some of the inputs but not necessarily all of them together...

--- UFO/static/js/app.js
+++ UFO/static/js/app.js
@@ -1,20 +1,25 @@
-// from data.js
-var tableData = data;
+// Assign the data from `data.js`
+var ufoData = data;
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-data.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+function appendTable(arr) {
+    arr.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.values(sighting).forEach(value => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
     });
-});
+}
+
+// Append all data into a table as the default view
+appendTable(ufoData);
 
 
 // ----------------------------------------------
-// Create event listeners to find rows that match user input as filters.
+// Create event listeners to find rows that match user input filters.
 
 // Select the button
 var button = d3.select("#filter-btn");
@@ -22,51 +27,44 @@ var button = d3.select("#filter-btn");
 // Select the form
 // var form = d3.select("form");
 
-// // Create event handlers 
-// button.on("click", runEnter);
+// Create event handlers 
+button.on("click", runEnter);
 // form.on("submit",runEnter);
 
-// // Complete the event handler function for the form
-// function runEnter() {
+// Complete the event handler function for the form
+function runEnter() {
 
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
+  // Prevent the page from refreshing
+    // d3.event.preventDefault();
   
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#datetime");
+    // Get the value property of the `datetime` input element
+    var inputDate = d3.select("#datetime").property("value");
+    // Get the value property of the `city` input element
+    var inputCity = d3.select("#city").property("value");
+    // Get the value property of the `state` input element
+    var inputState = d3.select("#state").property("value");
+    // Get the value property of the `country` input element
+    var inputCountry = d3.select("#country").property("value");
+    // Get the value property of the `shape` input element
+    var inputShape = d3.select("#shape").property("value");
 
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
+    console.log(inputDate);
 
-//   console.log(inputValue);
-//   console.log(people);
 
-//   var filteredData = people.filter(person => person.bloodType === inputValue);
+    // Create filters to filter through all eligible inputs
+    var filteredData = ufoData.filter(
+        sighting => 
+        sighting.datetime === inputDate &&
+        sighting.city === inputCity &&
+        sighting.state === inputState &&
+        sighting.country === inputCountry &&
+        sighting.shape === inputShape
+        );
 
-//   console.log(filteredData);
+    console.log(filteredData);
 
-//   // BONUS: Calculate summary statistics for the age field of the filtered data
+    tbody.html("");
 
-//   // First, create an array with just the age values
-//   var ages = filteredData.map(person => person.age);
+    appendTable(filteredData);
+};
 
-//   // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-//   var mean = math.mean(ages);
-//   var median = math.median(ages);
-//   var mode = math.mode(ages);
-//   var variance = math.variance(ages);
-//   var standardDeviation = math.std(ages);
-
-//   // Then, select the unordered list element by class name
-//   var list = d3.select(".summary");
-
-//   // remove any children from the list to
-//   list.html("");
-
-//   // append stats to the list
-//   list.append("li").text(`Mean: ${mean}`);
-//   list.append("li").text(`Median: ${median}`);
-//   list.append("li").text(`Mode: ${mode}`);
-//   list.append("li").text(`Variance: ${variance}`);
-//   list.append("li").text(`Standard Deviation: ${standardDeviation}`);
-// };
